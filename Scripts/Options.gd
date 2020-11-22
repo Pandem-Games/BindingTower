0dea865e21bb5522fe85aa4e70bf6e29cadf77c6
TrevorSpear <spear367@gmail.com>
2020-11-22 14:54:32 -0800

    Setting basics are finished

--- Scripts/Options.gd
+++ Scripts/Options.gd
@@ -1,47 +1,133 @@
 extends Control
 
-enum eVideo { ONHOVER, FINISHED}
-enum eAudio { ONHOVER, FINISHED}
-enum eControls { ONHOVER, INISHED}
-enum eBack { ONHOVER, FINISHED}
-var state = eVideo.FINISHED
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var all_options = {
+	"Resolution" : {
+		"1920 x 1080" : {
+			"width" : 1920,
+			"height" : 1080
+		},
+		"1680 x 1050" : {
+			"width" : 1680,
+			"height" : 1050
+		},
+		"1600 x 900" : {
+			"width" : 1600,
+			"height" : 900
+		},
+		"1536 x 864" : {
+			"width" : 1536,
+			"height" : 864
+		},
+		"1440 x 1080" : {
+			"width" : 1440,
+			"height" : 1080
+		},
+		"1400 x 1050" : {
+			"width" : 1400,
+			"height" : 1050
+		},
+		"1366 x 768" : {
+			"width" : 1366,
+			"height" : 768
+		},
+		"1280 x 960" : {
+			"width" : 1280,
+			"height" : 960
+		},
+		"1280 x 720" : {
+			"width" : 1280,
+			"height" : 720
+		},
+		"1024 x 768" : {
+			"width" : 1024,
+			"height" : 768
+		},
+		"1024 x 640" : {
+			"width" : 1024,
+			"height" : 640
+		},
+		"1014 x 576" : {
+			"width" : 1014,
+			"height" : 576
+		},
+		"800 x 600" : {
+			"width" : 800,
+			"height" : 600
+		}
+	},
+#	"Fullscreen" : {
+#		"fullscreen" : "fullscreen",
+#		"windowed" : "windowed",
+#		"boarderless" : "boarderless"
+#	}
 
+}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$MarginContainer/HBoxContainer/btn_video.connect("pressed", self, "Video")
-	$MarginContainer/HBoxContainer/btn_audio.connect("pressed", self, "Audio")
-	$MarginContainer/HBoxContainer/btn_controls.connect("pressed", self, "Controls")
-	$btn_back.connect("pressed", self, "Back")
-	
-	#$MarginContainer/HBoxContainer/Menu/btn_video.connect("mouse_entered", self, "change_color_state", [eVideo, eVideo.ONHOVER])
-	#$MarginContainer/HBoxContainer/Menu/btn_audio.connect("mouse_entered", self, "change_color_state", [eAudio, eAudio.ONHOVER])
-	#$MarginContainer/HBoxContainer/Menu/btn_controls.connect("mouse_entered", self, "change_color_state", [eControls, eControls.ONHOVER])
-	#$MarginContainer/HBoxContainer/Menu/btn_back.connect("mouse_entered", self, "change_color_state", [eBack, eBack.ONHOVER])
-	
-	#$MarginContainer/HBoxContainer/Menu/btn_video.connect("mouse_exited", self, "change_color_state", [eVideo, eVideo.FINISHED])
-	#$MarginContainer/HBoxContainer/Menu/btn_audio.connect("mouse_exited", self, "change_color_state", [eAudio, eAudio.FINISHED])
-	#$MarginContainer/HBoxContainer/Menu/btn_controls.connect("mouse_exited", self, "change_color_state", [eControls, eControls.FINISHED])
-	#$MarginContainer/HBoxContainer/Menu/btn_back.connect("mouse_exited", self, "change_color_state", [eBack, eBack.FINISHED])
-	
-	pass # Replace with function body.
+	# Call this to hid ethe other options and just display video options
+	Video()
 
+	for i in all_options:
+		call(i, all_options[i])
+
+	set_volume()
 
 
 func Video():
-	pass
+	$VBoxContainer/Video.set_visible(true);
+	$VBoxContainer/Audio.set_visible(false);
+	$VBoxContainer/Controls.set_visible(false);
+
 
 func Audio():
-	pass
+	$VBoxContainer/Video.set_visible(false);
+	$VBoxContainer/Audio.set_visible(true);
+	$VBoxContainer/Controls.set_visible(false);
+
 
 func Controls():
-	pass
+	$VBoxContainer/Video.set_visible(false);
+	$VBoxContainer/Audio.set_visible(false);
+	$VBoxContainer/Controls.set_visible(true);
+
 
 func Back():
 	get_tree().change_scene("res://Nodes/Scenes/Menu/TitleScreen.tscn")
-	pass
 
+
+func Resolution(item):
+	var counter = 0
+
+	for i in item:
+		$VBoxContainer/Video/btn_resolution.add_item(i, counter)
+
+		if Settings.settings["res_width"] == all_options["Resolution"][i]["width"] and Settings.settings["res_height"] == all_options["Resolution"][i]["height"]:
+			$VBoxContainer/Video/btn_resolution.selected = counter
+
+		counter += 1
+
+
+func resolution_selected(item_id):
+	var key = all_options["Resolution"].keys()[item_id]
+	Settings.set_resolution(all_options["Resolution"][key]["width"], all_options["Resolution"][key]["height"])
+	Settings.save_game()
+
+
+func Fullscreen(item):
+	var counter = 0
+
+	for i in item:
+		$VBoxContainer/Video/btn_fullscreen.add_item(i, counter)
+		counter += 1
+
+
+func volume_slider_changed(value, slider_name):
+	Settings.set_volume(value, slider_name)
+
+
+func set_volume():
+	$VBoxContainer/Audio/master_volume_slider.value = float(Settings.settings["Master_Volume"])
+	$VBoxContainer/Audio/music_volume_slider.value = float(Settings.settings["Music_Volume"])
+	$VBoxContainer/Audio/effects_volume_slider.value = float(Settings.settings["Effects_Volume"])
--- Scripts/TitleScreen.gd
+++ Scripts/TitleScreen.gd
@@ -1,59 +1,24 @@
 extends Control
 
-enum eContinue { ONHOVER, FINISHED}
-enum eNewGame { ONHOVER, FINISHED}
-enum eOptions { ONHOVER, FINISHED}
-enum eQuit { ONHOVER, FINISHED}
-var state = eContinue.FINISHED
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$MarginContainer/HBoxContainer/Menu/btn_continue.connect("pressed", self, "Continue")
-	$MarginContainer/HBoxContainer/Menu/btn_newGame.connect("pressed", self, "NewGame")
-	$MarginContainer/HBoxContainer/Menu/btn_options.connect("pressed", self, "Options")
-	$MarginContainer/HBoxContainer/Menu/btn_quit.connect("pressed", self, "Quit")
-	
-	#$MarginContainer/HBoxContainer/Menu/btn_continue.connect("mouse_entered", self, "change_color_state", [eContinue, eContinue.ONHOVER])
-	#$MarginContainer/HBoxContainer/Menu/btn_newGame.connect("mouse_entered", self, "change_color_state", [eNewGame, eNewGame.ONHOVER])
-	#$MarginContainer/HBoxContainer/Menu/btn_options.connect("mouse_entered", self, "change_color_state", [eOptions, eOptions.ONHOVER])
-	#$MarginContainer/HBoxContainer/Menu/btn_quit.connect("mouse_entered", self, "change_color_state", [eQuit, eQuit.ONHOVER])
-	
-	#$MarginContainer/HBoxContainer/Menu/btn_continue.connect("mouse_exited", self, "change_color_state", [eContinue, eContinue.FINISHED])
-	#$MarginContainer/HBoxContainer/Menu/btn_newGame.connect("mouse_exited", self, "change_color_state", [eNewGame, eNewGame.FINISHED])
-	#$MarginContainer/HBoxContainer/Menu/btn_options.connect("mouse_exited", self, "change_color_state", [eOptions, eOptions.FINISHED])
-	#$MarginContainer/HBoxContainer/Menu/btn_quit.connect("mouse_exited", self, "change_color_state", [eQuit, eQuit.FINISHED])
-	
 	pass # Replace with function body.
 
-func change_color_state(state_given, changed_state):
-	#print(state_given)
-	match changed_state:
-		state_given.ONHOVER:
-			print("onhover")
-			#change the color to yellow
-		
-		state_given.FINISHED:
-			print("finished")
-			# return color back to normal
-	pass
 
 func Continue():
 	get_tree().change_scene("res://Nodes/Scenes/Test.tscn")
 	pass
 
+
 func NewGame():
 	Continue()
+
 
 func Options():
 	get_tree().change_scene("res://Nodes/Scenes/Options.tscn")
 	pass
 
+
 func Quit():
 	get_tree().quit()
 	pass
-
