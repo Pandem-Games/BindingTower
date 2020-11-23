b737e9af11baeace642f0600a1d36ef980dddcaa
TrevorSpear <spear367@gmail.com>
2020-11-23 11:11:05 -0800

    adding mouse click to ui_select

--- Scripts/TowerButton.gd
+++ Scripts/TowerButton.gd
@@ -31,19 +31,26 @@ func select():
 
 # Used to detect when user has clicked the button
 func _on_Tower_gui_input(event: InputEvent):
+	print("1")
 	# This event is also raised by the tower when the user clicks so we have to 
 	# check that the user is clicking on the tower button
+	print(str(event.is_action_pressed("ui_select")))
+	print(str(get_rect().has_point(get_local_mouse_position())))
 	if event.is_action_pressed("ui_select") and get_rect().has_point(get_local_mouse_position()):
+		print("5")
 		match state:
 			# The user is clicking on the tower button, initiating the selection
 			eTowerButton.WAIT:
 				select()
 				accept_event()
+				print("2")
 			# The user is clicking on the tower button again, cancelling the selection
 			eTowerButton.SELECTED:
 				emit_signal(Constants.TOWER_PLACEMENT_CANCELLED)
 				state = eTowerButton.WAIT
 				accept_event()
+				print("3")
+	print("4")
 
 # Called when a tower is placed successfully, which resets the tower button
 func _on_Tower_placement_confirmed():
