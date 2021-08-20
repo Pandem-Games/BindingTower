d11b5f99fb9822ee8c0582b74cd32faa6abbc489
vandomej <jacob.vandome15@gmail.com>
2021-08-20 09:43:13 -0700

    Finished implementing interactability for items

--- Main/Item/Item.gd
+++ Main/Item/Item.gd
@@ -7,34 +7,67 @@ enum eItem {DROPPED, GRABBABLE, ACTIVE, FINISHED}
 var state: int = eItem.DROPPED
 
 # Variables
+
+# Used to handle float amplitude and starting values for the floating animation
 export(float) var float_amount := 1.0
 export(float) var initial_float_height := 0.5
-onready var control: Control = $Control
-onready var audio: AudioStreamPlayer = $PickupSound
-onready var item_shadow: AnimatedSprite = $ItemShadow
-var item_count: int = 1
 var current_time: float = 0.0
 var animation_length: float = 0.0
 
+onready var interact_label: RichTextLabel = $Control/Label
+onready var audio: AudioStreamPlayer = $PickupSound
+onready var item_shadow: AnimatedSprite = $ItemShadow
+
+# Keeps track of how many of this item has been collected
+var item_count: int = 1
+
 # Functions
 func _ready() -> void:
+	# When item is spawned it is dropped and visible
 	visible = true
-	state = eItem.DROPPED
+	dropped()
 	animation_length = Helpers.get_animation_length(item_shadow.frames, "default")
 	current_time = initial_float_height / animation_length
 	
 func _process(delta: float) -> void:
 	match state:
-		eItem.DROPPED:
+		eItem.DROPPED, eItem.GRABBABLE:
+			# Synchronizing the floating animation with the drop shadow
 			var height: float = sin(current_time * ((2 * PI) / animation_length))
 			offset.y = height * float_amount
 			current_time += delta
-	
+
+# Checking if object has been interacted with
+func _unhandled_key_input(event: InputEventKey) -> void:
+	if event.is_action_pressed("interact"):
+		if state == eItem.GRABBABLE:
+			visible = false
+			audio.play()
+			yield(audio, "finished")
+			Helpers.call_error_function(Helpers, "add_item", [self])
+			active()
+
+# State transition to grabbable
+func grabbable() -> void:
+	interact_label.visible = true
+	state = eItem.GRABBABLE
+
+# State transition to dropped
+func dropped() -> void:
+	interact_label.visible = false
+	state = eItem.DROPPED
+
+# State transition to active
+func active() -> void:
+	interact_label.visible = false
+	state = eItem.ACTIVE
+
+# State transition to finish
 func finish() -> void:
 	state = eItem.FINISHED
 	queue_free()
 	
-# Virtual functions to override
+# Virtual functions to override for inheriting classes
 func _item_initial(_tower: Node2D) -> void:
 	pass
 
@@ -47,20 +80,11 @@ func _item_on_bullet_processed(_bullet: Node2D) -> void:
 func _item_on_hit(_bullet: Node2D, _target: Node2D) -> void:
 	pass
 
-func _on_Control_gui_input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_select"):
-		match state:
-			eItem.DROPPED:
-				visible = false
-				audio.play()
-				yield(audio, "finished")
-				Helpers.call_error_function(Helpers, "add_item", [self])
-				state = eItem.ACTIVE
-				Helpers.safe_disconnect(control, "gui_input", self, "_on_Control_gui_input")
-
+# Signal functions waiting for player to enter interactable range of item.
 func _on_Area_body_entered(_body):
-	state = eItem.GRABBABLE
+	if state == eItem.DROPPED:
+		grabbable()
 
 func _on_Area_body_exited(_body):
 	if state == eItem.GRABBABLE:
-		state = eItem.DROPPED
+		dropped()
